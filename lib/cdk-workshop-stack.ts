174952b2f3623df01d64b6bf681c284ae82307d8
Vignesh <vignesh.r@foodhub.com>
2025-12-04 15:10:41 +0530

    feat: crud operations in s3 using lambda and api gateway

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,25 +1,53 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib/core';
-import {Function, Code, Runtime} from 'aws-cdk-lib/aws-lambda'
-import { Construct } from 'constructs';
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import { HitCounter } from './hitcounter';
+import {
+  CfnOutput,
+  Duration,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib/core";
+import { Function, Code, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import {
+  LambdaIntegration,
+  LambdaRestApi,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    const hello = new Function(this, "HelloHandler", {
-      runtime: Runtime.NODEJS_22_X,
-      code: Code.fromAsset('lambda'),
-      handler: 'hello.handler'
-    })
-
-    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
-      downstream: hello,
+    const bucket = new Bucket(this, "CdkCrudBucket", {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
-    // defines an API Gateway REST API resource backed by our "hello" function.
-    const gateway = new LambdaRestApi(this, "Endpoint", {
-      handler: helloWithCounter.handler,
+    const func = new Function(this, "CdkS3CrudHandler", {
+      runtime: Runtime.NODEJS_22_X,
+      code: Code.fromAsset("lambda"),
+      handler: "bucket.handler",
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+    });
+
+    bucket.grantReadWrite(func);
+
+    const api = new RestApi(this, "CdkS3CrudApi");
+
+    const object = api.root.addResource("object");
+    const objectKey = object.addResource("{key}");
+
+    objectKey.addMethod("POST", new LambdaIntegration(func));
+    objectKey.addMethod("GET", new LambdaIntegration(func));
+    objectKey.addMethod("DELETE", new LambdaIntegration(func));
+
+    new CfnOutput(this, "ApiUrl", {
+      value: api.url,
+    });
+
+    new CfnOutput(this, "BucketName", {
+      value: bucket.bucketName,
     });
   }
 }
